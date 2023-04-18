462c3275d4f0892cfcd7e6acb4e1756cfc6077db
DawnKosmos <martin_hottmann@yahoo.com>
2023-04-18 19:48:55 +0200

    added add Order

--- kapi/user.go
+++ kapi/user.go
@@ -2,11 +2,11 @@ package kapi
 
 type AddOrderRequest struct {
 	Pair       string `url:"pair"`
-	Type       string `url:"type"`
+	Type       string `url:"type"`            // Buy, Sell
 	Ordertype  string `url:"ordertype"`       // limit, market
 	Price      string `url:"price,omitempty"` //
 	Volume     string `url:"volume"`
-	Leverage   string `url:"leverage,omitempty"`
+	Leverage   string `url:"leverage,omitempty"` // 2:1
 	ReduceOnly bool   `url:"reduce_only,omitempty"`
 }
 
--- user_test.go
+++ user_test.go
@@ -2,12 +2,13 @@ package kraken
 
 import (
 	"fmt"
+	"github.com/DawnKosmos/kraken-go/kapi"
 	"testing"
 )
 
 func TestAccountBalance(t *testing.T) {
 	cl, err := New(nil, &Account{
-		PublicKey: "",
+		PublicKey: "NgXwclr91aZZGoDr5u8+Zvct4JxKV5GzrxN81od/Dc1EbQTG3n9swuO4",
 		SecretKey: "",
 	}, true)
 
@@ -17,4 +18,25 @@ func TestAccountBalance(t *testing.T) {
 		t.Fail()
 	}
 	fmt.Println(resp)
+}
+
+func TestAddOrder(t *testing.T) {
+	cl, err := New(nil, &Account{
+		PublicKey: "NgXwclr91aZZGoDr5u8+Zvct4JxKV5GzrxN81od/Dc1EbQTG3n9swuO4",
+		SecretKey: "",
+	}, true)
+
+	resp, err := cl.AddOrder(kapi.AddOrderRequest{
+		Pair:      "SOLUSD",
+		Type:      "sell",
+		Ordertype: "limit",
+		Price:     "30.00",
+		Volume:    "0.5",
+	})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	fmt.Println(resp)
+
 }
